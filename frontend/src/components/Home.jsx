d3e7f8ac36462037846813a0df56f2c50e78b59a
Devesh Shukla <deveshshukla1603@gmail.com>
2024-05-27 22:59:45 +0530

    Postfrontend

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Createpost from './Createpost'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import Posts from './Posts';
+import axios from 'axios';
+import loading from '../redux/loading';
 
 
 const Home = () => {
   const isLogin= useSelector(state=> state.login.value);
+  const dispatch = useDispatch();
+  const [posts , setPosts] = useState([]);
+  const getPost = async()=>{
+    try {
+      const res = await axios.get('http://localhost:3000/posts/getPost');
+      setPosts(res.data.posts);
+      if(res.status==200)
+        {
+        }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  useEffect(() => {
+    // dispatch(loading());
+    getPost();
+    // dispatch(loading())
+    
+  }, [])
+  console.log(posts);
   return (
     <div className=' h-full overflow-auto border-x-2 border-black pl-16'>
         
@@ -13,8 +35,10 @@ const Home = () => {
         <div className='bg-black h-[0.5px]'></div>
 
         <div className="post ">
-          <Posts/>
-        
+        {posts?.map((post)=>{
+          return <Posts key={post.id} username={post.userId} title={post.title} body={post.body} media={post.img}/>
+
+        })}
           <div className='bg-black h-[0.5px]'></div>
 
         </div>
